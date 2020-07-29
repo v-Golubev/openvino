acb8e65776bde9a6560a55588920a71413a3e283
Golubev <vladislav.golubev@intel.com>
2020-07-29 12:43:17 +0300

    [LPT] Split: fixed generateInput

--- inference-engine/tests/functional/plugin/shared/src/low_precision_transformations/split_transformation.cpp
+++ inference-engine/tests/functional/plugin/shared/src/low_precision_transformations/split_transformation.cpp
@@ -41,8 +41,8 @@ InferenceEngine::Blob::Ptr SplitTransformation::GenerateInput(const InferenceEng
 
     return FuncTestUtils::createAndFillBlobConsistently(
         info.getTensorDesc(),
-        fqOnData.empty() ? 25.f : fqOnData.outputHighValues[0] - fqOnData.outputLowValues[0],
-        fqOnData.empty() ? -12.5f : fqOnData.outputLowValues[0],
+        static_cast<uint32_t>(fqOnData.empty() ? 25.f : fqOnData.outputHighValues[0] - fqOnData.outputLowValues[0]),
+        static_cast<int32_t>(fqOnData.empty() ? -12.5f : fqOnData.outputLowValues[0]),
         1ul);
 }
 
