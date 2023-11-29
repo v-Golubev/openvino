f6caa454f49900bc9d8771f8bd926d8d5640ace4
Alexandra Sidorova <alexandra.sidorova@intel.com>
2023-11-29 17:41:58 +0400

    [Snippets] Added PassConfig and PositionedPasses for lowered PassPipeline
    [Snippets] Added PassConfig for lowered pipeline
    
    [Snippets] Added position support for lowered passes
    
    [Snippets][CPU] Added config and backed positioned pass support to CPU Plugin
    
    small fixes
    
    [Snippets] Changed string to DiscreteTypeInfo in snippets manager

--- src/common/snippets/include/snippets/op/subgraph.hpp
+++ src/common/snippets/include/snippets/op/subgraph.hpp
@@ -104,13 +104,13 @@ public:
                                 const std::vector<ov::element::Type>& input_precisions = {},
                                 const std::vector<ov::element::Type>& output_precisions = {},
                                 const std::vector<pass::Manager::PositionedPass>& data_flow_passes = {},
-                                const lowered::pass::PassPipeline& control_flow_passes_pre_common = {},
-                                const lowered::pass::PassPipeline& control_flow_passes_post_common = {},
+                                const std::shared_ptr<lowered::pass::PassConfig>& lowered_pass_config = nullptr,
+                                const std::vector<lowered::pass::PassPipeline::PositionedPass>& lowered_backend_passes = {},
                                 const std::shared_ptr<IShapeInferSnippetsFactory>& factory = nullptr,
                                 const void* compile_params = nullptr);
 
-    snippets::Schedule generate_from_linear_ir(const lowered::pass::PassPipeline& backend_passes_pre_common = {},
-                                               const lowered::pass::PassPipeline& backend_passes_post_common = {},
+    snippets::Schedule generate_from_linear_ir(const std::shared_ptr<lowered::pass::PassConfig>& lowered_pass_config = nullptr,
+                                               const std::vector<lowered::pass::PassPipeline::PositionedPass>& lowered_backend_passes = {},
                                                const void* compile_params = nullptr) const;
     IShapeInferSnippets::Result shape_infer(const std::vector<VectorDimsRef>& input_shapes);
 
@@ -149,8 +149,8 @@ public:
 private:
     void control_flow_transformations(lowered::LinearIR& linear_ir,
                                       LoweringResult& lowering_result,
-                                      const lowered::pass::PassPipeline& backend_passes_pre_common,
-                                      const lowered::pass::PassPipeline& backend_passes_post_common) const;
+                                      const std::shared_ptr<lowered::pass::PassConfig>& lowered_pass_config = nullptr,
+                                      const std::vector<lowered::pass::PassPipeline::PositionedPass>& lowered_backend_passes = {}) const;
     void init_config();
     // Count of Subgraph virtual ports:
     //  - Potential non-scalar Constants that will be created after some transformations (At the moment it's relevant only for FakeQuantize decomposition)
--- src/common/snippets/tests/include/lowering_utils.hpp
+++ src/common/snippets/tests/include/lowering_utils.hpp
@@ -62,8 +62,8 @@ public:
             getLoweredSubgraph(const std::shared_ptr<Model>& f,
                                const ov::PartialShape& master_shape,
                                const std::vector<ov::snippets::pass::Manager::PositionedPass>& backend_passes = {},
-                               const ov::snippets::lowered::pass::PassPipeline& lowered_pre_common = {},
-                               const ov::snippets::lowered::pass::PassPipeline& lowered_post_common = {},
+                               const std::shared_ptr<ov::snippets::lowered::pass::PassConfig>& lowered_pass_config = nullptr,
+                               const std::vector<ov::snippets::lowered::pass::PassPipeline::PositionedPass>& lowered_backend_passes = {},
                                const std::shared_ptr<ov::snippets::Generator>& generator = nullptr,
                                const std::shared_ptr<IShapeInferSnippetsFactory>& factory = std::make_shared<IShapeInferSnippetsFactory>());
     static std::shared_ptr<ov::snippets::op::Subgraph> getTokenizedSubgraph(const std::shared_ptr<Model>& f);
